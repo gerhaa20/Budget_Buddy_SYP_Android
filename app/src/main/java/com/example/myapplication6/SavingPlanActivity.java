e59ba4f6b053d003c907ae868f47335fade7954e
Ryan <zanrya19@htl-kaindorf.at>
2024-01-11 15:31:08 +0100

    Add Savings hinzugefügt

--- app/src/main/java/com/example/myapplication6/SavingPlanActivity.java
+++ app/src/main/java/com/example/myapplication6/SavingPlanActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SavingPlanActivity extends AppCompatActivity {
+
+    Button addSaving;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saving_plan);
+
+        addSaving = findViewById(R.id.btnAddSaving);
+        addSaving.setOnClickListener(view -> openAddSavings());
+    }
+
+    public void openAddSavings(){
+        try {
+            Intent intent = new Intent(this,AddSavingPlan.class);
+            startActivity(intent);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
     }
 }
